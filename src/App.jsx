04875eac2750844c4e3f898168cb9cc1d5f9ae90
박태근 <taegeun1234@naver.com>
2025-09-10 20:59:55 +0900

    🎉 Start: 초기 설정 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import RootLayout from "./shared/layouts/RootLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ServiceLayout from "./shared/layouts/ServiceLayout";
+
+import Home from "./routes/home";
+
+const router = createBrowserRouter([
+  {
+    Component: RootLayout,
+    children: [
+      { path: "/", index: true, Component: Home },
+      {
+        Component: ServiceLayout,
+        children: [
+          // { path: "", Component:  },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1,
+      },
+    },
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
