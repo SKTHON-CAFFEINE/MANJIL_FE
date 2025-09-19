4803024ba4db330de57601683143c1b3a55792fb
박태근 <taegeun1234@naver.com>
2025-09-19 14:37:13 +0900

    ✨Feat: 레이아웃 구조 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,48 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
-import RootLayout from "./shared/layouts/RootLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import RootLayout from "./shared/layouts/RootLayout";
 import ServiceLayout from "./shared/layouts/ServiceLayout";
 import ProtectedRoute from "./shared/layouts/ProtectedRoute";
 
 import RootPage from "./routes/home/index";
+// import LoginPage from "./routes/auth/LoginPage";
+// import SignupPage from "./routes/auth/SignupPage";
 
 const router = createBrowserRouter([
+  // 일반 서비스 브랜치 (RootLayout 사용)
   {
     Component: RootLayout,
     children: [
       { index: true, Component: RootPage },
-      
-      {
-        Component: ServiceLayout,
-        children: [
-          // 내 생각에 서비스 레이아웃에는 회원가입 & 로그인 관련 페이지들만 넣으면 될 듯..?
-          // { path: "", Component:  },
-        ],
-      },
-
-      // 토큰이 필요한 페이지들
       {
         Component: ProtectedRoute,
         children: [
-          // 홈 페이지
-          {
-            path: "/",
-            children: [
-              // 향후 추가될 페이지들
-              // { path: "", Component: },
-            ],
-          },
+          // 보호가 필요한 페이지들
+          // { path: "", Component:  },
         ],
       },
+      // 기타 공개 페이지들
     ],
   },
+
+  // 인증/회원가입 브랜치 (ServiceLayout 사용, RootLayout 미사용)
+  {
+    path: "/auth",
+    Component: ServiceLayout,
+    children: [
+      // { index: true, Component: LoginPage },
+      // { path: "", Component: LoginPage },
+      // { path: "", Component: SignupPage },
+    ],
+  },
+
 ]);
 
 function App() {
   const queryClient = new QueryClient({
     defaultOptions: {
-      queries: {
-        retry: 1,
-      },
+      queries: { retry: 1 },
     },
   });
 
