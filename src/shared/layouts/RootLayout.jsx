1db6da192cabd7f15fdc2f2e8b128abe4b4a37ad
박태근 <taegeun1234@naver.com>
2025-09-20 18:57:25 +0900

    ✨ Feat: 하단바 안 보이는 조건 수정

--- src/shared/layouts/RootLayout.jsx
+++ src/shared/layouts/RootLayout.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
+import { useLocation } from "react-router";
 import { Outlet, ScrollRestoration } from "react-router";
 import Navbar from "../commons/navbar";
 // import Header from "../commons/Header";
 
 export default function RootLayout() {
+  const { pathname } = useLocation();
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const hasToken = !!localStorage.getItem("accessToken");
+
+  const hideNav = !hasToken && pathname === "/";
 
   return (
     <main>
       {/* <Header /> */}
       <Outlet context={{ onModalChange: setIsModalOpen }} />
       <ScrollRestoration />
-      {!isModalOpen && <Navbar />}
+      {!isModalOpen && !hideNav && <Navbar />}
     </main>
   );
 }
